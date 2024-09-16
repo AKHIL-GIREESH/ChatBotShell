79a35b818c00444837a1f458cfb358751ad47d8f
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-09-16 16:28:31 +0530

    V1

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ const rl = readline.createInterface({
 
 const genAI = new GoogleGenerativeAI(process.env.KEY);
 
-//Text to Text
-
-async function Text2Text() {
+const Text2Text = async () => {
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
 
   let history = []
@@ -32,25 +30,9 @@ async function Text2Text() {
 
     console.log(prompt)
 
-    //const prompt = "INFJ & INTJ compatibility";
-
-    //const result = await model.generateContent(prompt);
-
     const result = await chat.sendMessage(" Answer without any bold charcters"+prompt)
     const response = await result.response;
     const text = response.text();
-
-    // history.push({
-    //   role:"user",
-    //   parts:[
-    //     {text:prompt}
-    //   ]
-    // })
-
-    // history.push({
-    //   role:"model",
-    //   parts:[{text:text}]
-    // })
 
     console.log(text+"\n\n");
     console.log(history)
@@ -64,57 +46,4 @@ async function Text2Text() {
   return
 }
 
-//Img/Text to Text
-
-function fileToGenerativePart(path, mimeType) {
-  return {
-    inlineData: {
-      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
-      mimeType,
-    },
-  };
-}
-
-async function Text_Img2Text() {
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-  const prompt =
-    "Explain about the dude who draw this. Also brief about the painting itself";
-
-  const imageParts = [fileToGenerativePart("img1.jpeg", "image/jpeg")];
-
-  const result = await model.generateContent([prompt, ...imageParts]);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-}
-
-//Text to Chat
-
-async function Text2Chat() {
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-  const chat = model.startChat({
-    history: [
-      {
-        role: "user",
-        parts: [
-          { text: "Hello, I am demisexual and sapiosexual. I am also an INFJ" },
-        ],
-      },
-      {
-        role: "model",
-        parts: [{ text: "God God God God" }],
-      },
-    ],
-  });
-
-  const msg = "What are the traits in an INFJ which'd influence it?";
-
-  const result = await chat.sendMessage(msg);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-}
-
 Text2Text();
-//Text_Img2Text();
-//Text2Chat();
